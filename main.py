add0c7eb68b579c17f28f0d34950dbfc06d56d90
Zack Ankner <zackankner@gmail.com>
2022-05-24 14:55:13 -0400

    finished template

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 import pytorch_lightning as pl
 
 from model import Model
+from loggers import BlankLogger
 import data
 
 SAVE_DIR = "ADD SAVE DIR!!!"
@@ -20,17 +21,19 @@ def train(args):
     callbacks = [
         pl.callbacks.ModelCheckpoint(dirpath=os.path.join(
             SAVE_DIR, args.model_name),
-                                     every_n_epochs=args.save_freq)
+                                     every_n_epochs=args.save_freq,
+                                     save_top_k=-1),
+        pl.callbacks.TQDMProgressBar(refresh_rate=20)
     ]
 
-    model = Model()
+    model = Model(args)
 
     train_loader = data.get_train_loader()
     val_loader = data.get_val_loader()
 
     trainer = pl.Trainer(gpus=args.gpus,
                          check_val_every_n_epoch=args.eval_freq,
-                         max_epochs=args.max_epochs,
+                         max_epochs=args.epochs,
                          logger=logger,
                          callbacks=callbacks)
     trainer.fit(model,
@@ -39,9 +42,7 @@ def train(args):
 
 
 def test(args):
-    logger = pl.loggers.WandbLogger(project=PROJECT_NAME,
-                                    entity=PROJECT_ENTITY,
-                                    name=args.save_name)
+    logger = BlankLogger()
 
     checkpoint = os.path.join(SAVE_DIR, args.checkpoint)
 
@@ -54,9 +55,9 @@ def test(args):
 
 
 def main(args):
-    if args.train:
+    if args.action == "train":
         train(args)
-    elif args.test:
+    elif args.action == "test":
         test(args)
     else:
         raise ValueError("Must either be in train or test mode")
@@ -71,21 +72,23 @@ if __name__ == "__main__":
     sp = parser.add_subparsers(dest="action")
 
     # Training hparams
-    train = sp.add_parser("train")
-    train.add_argument("--model-name", type=str, required=True)
-    train.add_argument("--trial", type=int, required=True)
-    train.add_argument("--seed", type=int, default=0)
-    train.add_argument("--save-freq", type=int, default=1)
-    train.add_argument("--eval-freq", type=int, default=1)
-    train.add_argument("--resume-path", type=str, default=None)
-    train.add_argument("--epochs", type=int, required=True)
-    train.add_argument("--learning-rate", type=float, default=1e-4)
-    train.add_argument("--weight-decay", type=float, default=0.0)
+    train_parser = sp.add_parser("train")
+    train_parser.add_argument("--model-name", type=str, required=True)
+    train_parser.add_argument("--seed", type=int, default=0)
+    train_parser.add_argument("--save-freq", type=int, default=1)
+    train_parser.add_argument("--eval-freq", type=int, default=1)
+    train_parser.add_argument("--resume-path", type=str, default=None)
+    train_parser.add_argument("--epochs", type=int, required=True)
+    train_parser.add_argument("--batch-size", type=int, required=True)
+    train_parser.add_argument("--learning-rate", type=float, default=1e-4)
+    train_parser.add_argument("--weight-decay", type=float, default=0.0)
+    train_parser.add_argument("--step-size", type=float, default=20)
+    train_parser.add_argument("--lr-decay", type=float, default=0.1)
 
     # Testing hparams
-    test = sp.add_parser("test")
-    test.add_argument("--save-name", type=str, required=True)
-    test.add_argument("--checkpoint", type=str, required=True)
+    test_parser = sp.add_parser("test")
+    test_parser.add_argument("--checkpoint", type=str, required=True)
+    test_parser.add_argument("--batch-size", type=int, default=64)
 
     args = parser.parse_args()
 
